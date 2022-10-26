383dcfba0ae64871bc277337507906506ec7d0de
nicola-19010 <n.pacheco02@ufromail.cl>
2022-10-26 10:55:44 -0300

    Commit 3(por si se me caba la bolsa de datos moviles)

---
+++ src/main/java/Mesero.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Mesero {
+	public ArrayList<Pedido> pedidoArrayList = new ArrayList<>();
+
+	private void atenderPedido() {
+		mostrarMenu();
+	}
+
+	private void mostrarMenu(){
+		System.out.println("Bienvenido, tenga el menu");
+		System.out.println("""
+				Platos principales:
+				1-x
+				2-y""");
+		System.out.println("""
+				Bebestibles:
+				1-x
+				2-y""");
+	}
+
+	private void PedirQueClienteDigaSuPedido(){
+		int  platoPrincipal = ingresar();
+		int  bebestible = ingresar();
+	}
+
+	public static int ingresar() {
+		Scanner teclado = new Scanner(System.in);
+		boolean esNumero = false;
+		int opcion = 0;
+
+		do {
+			try {
+				opcion = teclado.nextInt();
+				esNumero = true;
+			} catch (Exception e) {
+				teclado.next();
+				System.out.println("Ingrese una opcion valida del menu");
+			}
+		} while (!esNumero);
+
+		if (opcion < 1 || opcion > 2) {
+			System.out.println("Ingrese una opcion valida del menu");
+			return ingresar();
+		}
+		return opcion;
+	}
+
+	//Método para validar una entrada.
+
+
+}
---
+++ src/main/java/Pedido.java
@@ -0,0 +1,23 @@
+public class Pedido {
+	public Mesero _unnamed_Mesero_;
+	public Mesero _se_hace;
+
+	private String platoPrincipal;
+	private String bebestible;
+
+	public String getPlatoPrincipal() {
+		return platoPrincipal;
+	}
+
+	public void setPlatoPrincipal(String platoPrincipal) {
+		this.platoPrincipal = platoPrincipal;
+	}
+
+	public String getBebestible() {
+		return bebestible;
+	}
+
+	public void setBebestible(String bebestible) {
+		this.bebestible = bebestible;
+	}
+}
